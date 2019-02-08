7555b6e4c0c67d873688a1c6d94c7e0bdca878cf
Juan Cruz Leyba <jleyba@itx.com>
2019-02-08 19:32:04 -0300

    Removing search from cashout

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,5 +3,6 @@ const { mergeResolvers } = require('merge-graphql-schemas');
 const User = require('./userResolvers');
 const Product = require('./productResolvers');
 const Provider = require('./providerResolvers');
+const Cashout = require('./cashoutResolvers');
 
-module.exports = mergeResolvers([User, Product, Provider]);
+module.exports = mergeResolvers([User, Product, Provider, Cashout]);
