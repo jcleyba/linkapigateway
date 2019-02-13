096ba429bb1e308fa7de2db131f053a9e093b834
Juan Cruz Leyba <jleyba@itx.com>
2019-02-12 22:13:08 -0300

    Adding payment method files

---
+++ resolvers/paymentMethodResolvers.js
@@ -0,0 +1,16 @@
+const http = require('../http');
+
+const endpoint = '/paymentMethods';
+module.exports = {
+  Query: {
+    paymentmethods: async (parent, args, context) => {
+      try {
+        const { data } = await http.get(endpoint);
+        return data;
+      } catch (e) {
+        console.error(e.message);
+        return e;
+      }
+    },
+  },
+};
