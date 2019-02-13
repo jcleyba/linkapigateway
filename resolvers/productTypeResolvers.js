65cc0760402b6c4b3d9c947b66acb171b5454bc4
Juan Cruz Leyba <jleyba@itx.com>
2019-02-13 12:36:48 -0300

    Adding product types and resolvers

---
+++ resolvers/productTypeResolvers.js
@@ -0,0 +1,17 @@
+const http = require('../http');
+
+const endpoint = '/productsTypes';
+module.exports = {
+  Query: {
+    producttypes: async (parent, args, context) => {
+      try {
+        const { data } = await http.get(endpoint);
+
+        return data;
+      } catch (e) {
+        console.error(e.message);
+        return e;
+      }
+    },
+  },
+};
