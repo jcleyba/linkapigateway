07b2862cbf9607c0d32c52d8ddc0da3764b08aa8
ManuelMGM <manugm295@gmail.com>
2019-02-28 14:40:28 -0300

    query and resolver for saledetail

--- resolvers/index.js
+++ resolvers/index.js
@@ -5,6 +5,7 @@ const Product = require('./productsResolvers');
 const Provider = require('./providersResolvers');
 const Cashout = require('./cashOutsResolvers');
 const Sales = require('./salesResolvers');
+const SalesDetails = require('./salesDetailsResolvers');
 const PaymentMethods = require('./paymentMethodsResolvers');
 const CashShifts = require('./cashShiftsResolvers');
 const ProductTypes = require('./productTypesResolvers');
@@ -15,6 +16,7 @@ module.exports = mergeResolvers([
   Provider,
   Cashout,
   Sales,
+  SalesDetails,
   PaymentMethods,
   CashShifts,
   ProductTypes,
